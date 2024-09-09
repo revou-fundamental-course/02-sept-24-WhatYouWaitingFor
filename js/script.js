10899d2a7fce53fad997c36632eb54c55445c66c
Katon <99019267+WhatYouWaitingFor@users.noreply.github.com>
2024-09-09 22:28:23 +0700

    Terbaru

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-//ini javascript
+function peringatan(){
+    let panjangAlas = document.getElementById("panjang-alas").value;
+    let tinggi = document.getElementById("tinggi").value;
+    let result = parseInt(panjangAlas) + parseInt(tinggi);
+    document.write('Hasilnya adalah ' + result)
+}
