38cc1385d966cc8d98927d4c226edf9239396c7f
Katon <99019267+WhatYouWaitingFor@users.noreply.github.com>
2024-09-10 15:22:54 +0700

    Udah lumayan bagus

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ function peringatan(){
     document.write('Hasilnya adalah ' + result)
 }
 
+function keliling(){
+    let sisiA = document.getElementById("sisi-a").value;
+    let sisiB = document.getElementById("sisi-b").value;
+    let sisiC = document.getElementById("sisi-c").value;
+    let hasil = parseInt(sisiA) + parseInt(sisiB) + parseInt(sisiC);
+    document.write('Hasilnya adalah ' + hasil)}
+
