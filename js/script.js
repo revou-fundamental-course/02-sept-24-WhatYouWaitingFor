af7143fcf98a39e1a93346c76d9a6c95bf442e03
Katon <99019267+WhatYouWaitingFor@users.noreply.github.com>
2024-09-12 00:22:24 +0700

    cukup sempurna

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
-function peringatan(){
-    let panjangAlas = document.getElementById("panjang-alas").value;
-    let tinggi = document.getElementById("tinggi").value;
-    let result = parseInt(panjangAlas) + parseInt(tinggi);
-    document.write('Hasilnya adalah ' + result)
+/* memasukan fungsi logika perhitungan luas segitiga */
+function luas() {
+    const panjangAlas = parseFloat(document.getElementById('panjang-alas').value);
+    const tinggi = parseFloat(document.getElementById('tinggi').value);
+
+    if (isNaN(panjangAlas) || isNaN(tinggi)) {
+        alert('Harap masukkan semua nilai dengan benar.');
+        return;
+    }
+
+    const luas = 0.5 * panjangAlas * tinggi;
+
+    document.getElementById('result').textContent = `L = 1/2 x a x t = ${luas}`;
 }
 
-function keliling(){
-    let sisiA = document.getElementById("sisi-a").value;
-    let sisiB = document.getElementById("sisi-b").value;
-    let sisiC = document.getElementById("sisi-c").value;
-    let hasil = parseInt(sisiA) + parseInt(sisiB) + parseInt(sisiC);
-    document.write('Hasilnya adalah ' + hasil)}
 
+    /* memasukan fungsi logika perhitungan keliling segitiga */
+function keliling() {
+    const sisiA = parseFloat(document.getElementById('sisi-a').value);
+    const sisiB = parseFloat(document.getElementById('sisi-b').value);
+    const sisiC = parseFloat(document.getElementById('sisi-c').value);
+
+    if ( isNaN(sisiA) || isNaN(sisiB) || isNaN(sisiC)) {
+        alert('Harap masukkan semua nilai dengan benar.');
+        return;
+    }
+
+    const keliling = sisiA + sisiB + sisiC;
+
+    document.getElementById('hasil').textContent = `K = s + s + s = ${keliling}`;
+}
+
+/* memasukan fungsi hapus atau reset perhitungan */
+function resetluas() {
+    document.getElementById('panjang-alas').innerHTML = '';
+    document.getElementById('tinggi').innerHTML = '';
+    document.getElementById('result').innerHTML = '';
+}
